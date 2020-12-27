d3402ffd632ed4586c3ef29295ec5e3a9d3b27e4
k5liaaaan <65104856+k5liaaaan@users.noreply.github.com>
2020-12-27 16:12:04 +0900

    Add files via upload

---
+++ vol_02/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>PHP掲示板</title>
+<link rel="stylesheet" text="style/css" href="common.css">
+
+</head>
+<body>
+<h1>PHP掲示板</h1>
+<form method="post">
+	<div>
+		<label for="view_name">表示名</label>
+		<input id="view_name" type="text" name="view_name" value="">
+	</div>
+	<div>
+		<label for="message">メッセージ</label>
+		<textarea id="message" name="message"></textarea>
+	</div>
+	<input type="submit" name="btn_submit" value="書き込む">
+</form>
+<hr>
+<section>
+<!-- ここに投稿されたメッセージを表示 -->
+</section>
+</body>
+</html>
